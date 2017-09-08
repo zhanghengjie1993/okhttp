410f4951e584b756e5405916a37cbb6db42798f0
csdc01 <1920515424@qq.com>
2017-09-08 16:04:50 +0800

    测试crawler4j的用法

--- src/main/java/Appcontext.java
+++ src/main/java/Appcontext.java
@@ -1,13 +1,14 @@
-import hust.zhj.T;
 import hust.zhj.config.AppConfig;
 import hust.zhj.config.PersistenceJPAConfig;
-import hust.zhj.model.User;
-import hust.zhj.respository.UserRep;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import okhttp3.*;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.HashSet;
+import java.util.Set;
 /**
  * Created by zhanghj on 2017/8/1.
  */
@@ -16,19 +17,38 @@ public class Appcontext {
 
 //    private static final Logger LOGGER = LoggerFactory.getLogger(Appcontext.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
-//        ctx.scan("hust.zhj");
-        UserRep userRep = ctx.getBean(UserRep.class);
-        User user = new User();
-        user.setUsername("ASDdfsdfsadsasddfdf");
-        user.setPassword("password");
-        System.out.println(userRep.save(user).getId());
-        System.out.println("sucess");
-        T t = ctx.getBean(T.class);
-//        t.save();
-        Logger logger = LoggerFactory.getLogger(Appcontext.class);
-        logger.info("测试成功",t);
+        Set<String> numbers = new HashSet<String>();
+        String number;
+        do {
+            number = RandomStringUtils.random(8,false,true);
+        }
+        while(numbers.add(number)) ;
+        String name = "张三";
+        String mobile = "132" + number;
+        String channels = "0";
+        String validate = "3end";
+        OkHttpClient httpClient = new OkHttpClient();
+        for(int i = 0;i<=9999;i++){
+            int n = i+1;
+            DecimalFormat f = new DecimalFormat("0000");
+            String code = f.format(i);
+            RequestBody formBody = new FormBody.Builder()
+                    .add("mobile","15629173463")
+                    .add("name",name)
+                    .add("channels",channels)
+                    .add("code","6860")
+                    .add("validate",validate)
+                    .build();
+            Request request = new Request.Builder()
+                    .url("http://activity.yktour.com.cn/user/mobile/binding")
+                    .header("Cookie","DSESSIONID=a8bd8e57-7300-4adc-9a24-cc1dc85037bf")
+                    .post(formBody)
+                    .build();
+            Response response = httpClient.newCall(request).execute();
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            System.out.println("第"+n+"次尝试"+response.body().string());
+        }
     }
-
 }
--- src/main/java/hust/zhj/respository/UserRep.java
+++ src/main/java/hust/zhj/respository/UserRep.java
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRep extends JpaRepository<User, Long> {
     public User findByUserName(String username);
+
 }
